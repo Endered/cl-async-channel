e71e37a0b8e38dbd06243553c16b95f9fb7a1391
Endered <yy56ga10ve@gmail.com>
2023-01-31 01:28:45 +0900

    add package definition

--- src/cl-async-channel.lisp
+++ src/cl-async-channel.lisp
@@ -1,4 +1,13 @@
 (defpackage :cl-async-channel
-  (:use :cl))
-
-(in-package :cl-async-channel)
+  (:use :cl)
+  (:import-from
+   :cl-async-channel/channel
+   :make-channel
+   :send
+   :recv
+   :select)
+  (:export
+   :make-channel
+   :send
+   :recv
+   :select))
