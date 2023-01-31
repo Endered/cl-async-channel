914d0be55e5d96c128bcfab08cfb6e98411ef246
Endered <yy56ga10ve@gmail.com>
2023-01-31 16:39:28 +0900

    add utility macro

--- src/cl-async-channel.lisp
+++ src/cl-async-channel.lisp
@@ -10,4 +10,26 @@
    :make-channel
    :send
    :recv
-   :select))
+   :select
+
+   :>!
+   :<!))
+
+(in-package :cl-async-channel)
+
+(defmacro >! ((data channel &optional result) &body body)
+  (let ((x (or result (gensym))))
+    `(cl-async-channel:send
+      ,data
+      ,channel
+      (lambda (,x)
+	,@ (if result () `((declare (ignorable ,x))))
+	,@body))))
+
+(defmacro <! ((channel &optional result) &body body)
+  (let ((x (or result (gensym))))
+    `(cl-async-channel:recv
+      ,channel
+      (lambda (,x)
+	,@ (if result () `((declare (ignorable ,x))))
+	,@body))))
